359d65e284ee3fc8fec19e3edbdc6be61e686354
kaoru-drosera <fdsdteolx@gmail.com>
2021-06-21 22:41:27 +0900

    update: 16ボタン+音量調節、ループを実装。ただしここに「ループ再生時の再生形式変更」をしたい

--- js/lib.js
+++ js/lib.js
@@ -1,10 +1,99 @@
 console.log('hello,gulp.lol');
 
-var sound = new Audio();
+
+
+// 上段
+var soundQ = new Audio();
+var soundW = new Audio();
+var soundE = new Audio();
+var soundR = new Audio();
+soundQ.src = "./sounds/ラッパのファンファーレ.mp3";
+soundW.src = "./sounds/ドンドンパフパフ.mp3";
+soundE.src = "./sounds/レベルアップ.mp3";
+soundR.src = "./sounds/歓声と拍手.mp3";
+var volumeQ = document.getElementById("vol-q");
+var volumeW = document.getElementById("vol-w");
+var volumeE = document.getElementById("vol-e");
+var volumeR = document.getElementById("vol-r");
+var loopQ = document.getElementById("loopable_q");
+var loopW = document.getElementById("loopable_w");
+var loopE = document.getElementById("loopable_e");
+var loopR = document.getElementById("loopable_r");
+var volumeValue_q;
+var volumeValue_w;
+var volumeValue_e;
+var volumeValue_r;
+var loopValue_q;
+var loopValue_w;
+var loopValue_e;
+var loopValue_r;
+console.log(volumeQ);
+console.log(volumeW);
+console.log(volumeE);
+console.log(volumeR);
+
+
+// 中段
+var soundA = new Audio();
+var soundS = new Audio();
+var soundD = new Audio();
+var soundF = new Audio();
+soundA.src = "./sounds/和太鼓でドン.mp3";
+soundS.src = "./sounds/和太鼓でドドン.mp3";
+soundD.src = "./sounds/ピコピコハンマー.mp3";
+soundF.src = "./sounds/チーン1.mp3";
+var volumeA = document.getElementById("vol-a");
+var volumeS = document.getElementById("vol-s");
 var volumeD = document.getElementById("vol-d");
 var volumeF = document.getElementById("vol-f");
+var loopA = document.getElementById("loopable_a");
+var loopS = document.getElementById("loopable_s");
+var loopD = document.getElementById("loopable_d");
+var loopF = document.getElementById("loopable_f");
+var volumeValue_a;
+var volumeValue_s;
+var volumeValue_d;
+var volumeValue_f;
+var loopValue_a;
+var loopValue_s;
+var loopValue_d;
+var loopValue_f;
+console.log(volumeA);
+console.log(volumeS);
 console.log(volumeD);
 console.log(volumeF);
+
+// 下段
+var soundZ = new Audio();
+var soundX = new Audio();
+var soundC = new Audio();
+var soundV = new Audio();
+soundZ.src = "./sounds/クイズ出題1.mp3";
+soundX.src = "./sounds/決定、ボタン押下22.mp3";
+soundC.src = "./sounds/クイズ不正解1.mp3";
+soundV.src = "./sounds/クイズ正解1.mp3";
+var volumeZ = document.getElementById("vol-z");
+var volumeX = document.getElementById("vol-x");
+var volumeC = document.getElementById("vol-c");
+var volumeV = document.getElementById("vol-v");
+var loopZ = document.getElementById("loopable_z");
+var loopX = document.getElementById("loopable_x");
+var loopC = document.getElementById("loopable_c");
+var loopV = document.getElementById("loopable_v");
+var volumeValue_z;
+var volumeValue_x;
+var volumeValue_c;
+var volumeValue_v;
+var loopValue_z;
+var loopValue_x;
+var loopValue_c;
+var loopValue_v;
+console.log(volumeZ);
+console.log(volumeX);
+console.log(volumeC);
+console.log(volumeV);
+
+var sound = [soundQ, soundW, soundE, soundR, soundA, soundS, soundD, soundF, soundZ, soundX, soundC, soundV];
 
 // エラー抑制
 // https://qiita.com/krmbn0576/items/e9bc5384b790df9e39ab
@@ -14,38 +103,49 @@ console.log(volumeF);
 
 
 
-// クリックでの再生
-function disc(ele){
-  // const data = e.currentTarget.dataset['index'];
-  var attr = ele.getAttribute('data-sumplerBtn');
-  console.log(attr);
-
-  switch (attr) {
-    case 'd':
-      sound.src = "./sounds/click.mp3";
-      break;
-    case 'f':
-      sound.src = "./sounds/click2.mp3";
-      break;
-  };
-  play();
-
-};
+// // クリックでの再生
+// function disc(ele){
+//   // const data = e.currentTarget.dataset['index'];
+//   var attr = ele.getAttribute('keytouch');
+//   console.log(attr);
+//
+//   switch (attr) {
+//     case 'd':
+//     play(soundD);
+//       break;
+//     case 'f':
+//     play(soundF);
+//       break;
+//   };
+//
+// };
 
 // 初期化？
-function init(){
-  sound.preload = "auto";
-  // disc();
-  sound.src = "./sounds/click.mp3";
-  sound.load();
+var init = sound.map(function(value){
+    console.log(value);
+    value.preload = "auto";
+    value.load();
+  }
+);
 
+function volumeInit(soundName, volume, volumeValue){
   // 音量調節
-  volumeD.addEventListener('change', function () {
-    var volumeValue_d = (volumeD.value.length == 1) ? "0.0" + volumeD.value : "0." + volumeD.value ;
-    sound.volume = volumeValue_d;
+  volume.addEventListener('change', function () {
+    volumeValue = (volume.value.length == 1) ? "0.0" + volume.value : "0." + volume.value ;
+    soundName.volume = volumeValue;
   },false);
-
 }
+
+function loopInit(soundName, loop, loopValue){
+  // 音量調節
+  loop.addEventListener('change', function () {
+    loopValue = (loop.checked == true) ? loop.loop = true : loop.loop = false;
+    soundName.loop = loopValue;
+  },false);
+  console.log(soundD.loop);
+}
+console.log(soundD.loop);
+// console.log(loopValue_f);
 
 var playbtn = document.querySelectorAll('.playbtn');
 
@@ -54,43 +154,106 @@ document.body.addEventListener("keydown",
   event => {
     var whatIsKey = event.key;
     switch (whatIsKey) {
+
+      // 上段
+      case 'q':
+        play(soundQ);
+        console.log("q pressed");
+        break;
+      case 'w':
+        play(soundW);
+        console.log("w pressed");
+        break;
+      case 'e':
+        play(soundE);
+        console.log("e pressed");
+        break;
+      case 'r':
+        play(soundR);
+        console.log("r pressed");
+        break;
+
+      // 中段
+      case 'a':
+        play(soundA);
+        console.log("a pressed");
+        break;
+      case 's':
+        play(soundS);
+        console.log("s pressed");
+        break;
       case 'd':
-        sound.src = "./sounds/click.mp3";
+        play(soundD);
         console.log("d pressed");
         break;
       case 'f':
-        sound.src = "./sounds/click2.mp3";
+        play(soundF);
         console.log("f pressed");
         break;
+
+        // 下段
+        case 'z':
+          play(soundZ);
+          console.log("z pressed");
+          break;
+        case 'x':
+          play(soundX);
+          console.log("x pressed");
+          break;
+        case 'c':
+          play(soundC);
+          console.log("c pressed");
+          break;
+        case 'v':
+          play(soundV);
+          console.log("v pressed");
+          break;
+
     }
-    play();
-    // if(event.key === "d"){
-    //   console.log("d pressed");
-    //   play();
-    // } else if(event.key === "f") {
-    //   console.log("f pressed");
-    //   play();
-    // }
+
+
+
+
+
+//     play();
+//     // if(event.key === "d"){
+//     //   console.log("d pressed");
+//     //   play();
+//     // } else if(event.key === "f") {
+//     //   console.log("f pressed");
+//     //   play();
+//     // }
   }
 );
 
 
 
+// function loopable(checkbox = false){
+//   var bool;
+//   if(checkbox.checked){
+//     bool = true;
+//     // console.log(bool);
+//   } else {
+//     bool = false;
+//     // console.log(bool);
+//   }
+//   return bool;
+// }
 
-function play(){
-  // sound.loop = true;
-  sound.currentTime = 0;
-  sound.play();
+
+function play(plaing){
+    plaing.currentTime = 0;
+    plaing.play();
   // setTimeout(function(){
   //   sound.play();
   // },250);
-  const playPromise = sound.play();
-  if(playPromise !== null){
-    playPromise.catch(() => {sound.play();})
-  }
+  // const playPromise = sound.play();
+  // if(playPromise !== null){
+  //   playPromise.catch(() => {sound.play();})
+  // }
 };
 
 
 
 
-init();
+init;
